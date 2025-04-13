26f2bb99a5c841297c3f0604e80db4ae8d16d635
dimaakimm <dmitriykim2005@gmail.com>
2025-04-13 21:41:14 +0300

    added links to projects

--- src/atomic/pages/MainPage/MainPage.tsx
+++ src/atomic/pages/MainPage/MainPage.tsx
@@ -1,7 +1,26 @@
 import styles from './MainPage.module.scss'
+import { projects } from '../../../mockedData/mockedProjectData.ts'
+import Typography from '../../atoms/Typography/Typography.tsx'
+import Button from '../../atoms/Button/Button.tsx'
+import { useNavigate } from 'react-router-dom'
 
 const MainPage = () => {
-    return <div className={styles.wrapper}>mainPage</div>
+    const navigate = useNavigate()
+    return (
+        <div className={styles.wrapper}>
+            mainPage
+            <div className={styles.projects}>
+                {projects.map((project) => (
+                    <div>
+                        <Typography>{project.name}</Typography>
+                        <Button onClick={() => navigate(project.link)}>
+                            Перейти
+                        </Button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default MainPage
